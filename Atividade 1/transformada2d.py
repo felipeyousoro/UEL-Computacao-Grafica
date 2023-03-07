09d94ff50e07aa3184ba46f699547cd17dd7ec46
felipe based mito <42080804+felipeyousoro@users.noreply.github.com>
2023-03-07 15:53:47 -0300

    exercicios

--- Atividade 1/transformada2d.py
+++ Atividade 1/transformada2d.py
@@ -2,31 +2,21 @@ import numpy as np
 
 
 class Transformada2D:
-    def translacao(self, pontos, tx, ty):
+    def translacao(self, ponto, tx, ty):
         matriz_translacao = np.array([[1, 0, tx],
                                       [0, 1, ty],
                                       [0, 0, 1]], dtype=object)
 
-        _pontos = []
-        for ponto in pontos:
-            _pontos.append(np.dot(matriz_translacao, ponto))
+        return np.dot(matriz_translacao, ponto)
 
-        return _pontos
-
-    def escala(self, pontos, sx, sy):
+    def escala(self, ponto, sx, sy):
         matriz_escala = np.array([[sx, 0, 0],
                                   [0, sy, 0],
                                   [0, 0, 1]], dtype=object)
 
-        _pontos = []
-        for ponto in pontos:
-            _pontos.append(np.dot(matriz_escala, ponto))
+        return np.dot(matriz_escala, ponto)
 
-        return _pontos
-
-    def escalaCentroGeometrico(self, pontos, sx, sy):
-        centro = self.getCentroGeometrico(pontos)
-
+    def escalaCentroGeometrico(self, ponto, centro, sx, sy):
         matriz_translacao = np.array([[1, 0, centro[0]],
                                       [0, 1, centro[1]],
                                       [0, 0, 1]], dtype=object)
@@ -41,15 +31,11 @@ class Transformada2D:
 
         matriz_composta = np.dot(matriz_translacao, np.dot(matriz_escala, matriz_translacao_inversa))
 
-        _pontos = []
-        for ponto in pontos:
-            _pontos.append(np.dot(matriz_composta, ponto))
+        return np.dot(matriz_composta, ponto)
 
-        return _pontos
-
-    def escalaPonto(self, pontos, x, y, sx, sy):
-        matriz_translacao = np.array([[1, 0, x],
-                                      [0, 1, y],
+    def escalaPonto(self, ponto, referencia, sx, sy):
+        matriz_translacao = np.array([[1, 0, referencia[0]],
+                                      [0, 1, referencia[1]],
                                       [0, 0, 1]], dtype=object)
 
         matriz_escala = np.array([[sx, 0, 0],
@@ -62,35 +48,18 @@ class Transformada2D:
 
         matriz_composta = np.dot(matriz_translacao, np.dot(matriz_escala, matriz_translacao_inversa))
 
-        _pontos = []
-        for ponto in pontos:
-            _pontos.append(np.dot(matriz_composta, ponto))
+        return np.dot(matriz_composta, ponto)
 
-        return _pontos
-
-    def getCentroGeometrico(self, pontos):
-        x = 0
-        y = 0
-        for ponto in pontos:
-            x += ponto[0]
-            y += ponto[1]
-
-        return np.array([x / len(pontos), y / len(pontos)])
-
-    def rotacao(self, pontos, angulo):
+    def rotacao(self, ponto, angulo):
         matriz_rotacao = np.array([[np.cos(angulo), -np.sin(angulo), 0],
                                    [np.sin(angulo), np.cos(angulo), 0],
                                    [0, 0, 1]], dtype=object)
 
-        _pontos = []
-        for ponto in pontos:
-            _pontos.append(np.dot(matriz_rotacao, ponto))
+        return np.dot(matriz_rotacao, ponto)
 
-        return _pontos
-
-    def rotacaoPonto(self, pontos, x, y, angulo):
-        matriz_translacao = np.array([[1, 0, x],
-                                      [0, 1, y],
+    def rotacaoPonto(self, ponto, referencia, angulo):
+        matriz_translacao = np.array([[1, 0, referencia[0]],
+                                      [0, 1, referencia[1]],
                                       [0, 0, 1]], dtype=object)
 
         matriz_rotacao = np.array([[np.cos(angulo), -np.sin(angulo), 0],
@@ -103,24 +72,16 @@ class Transformada2D:
 
         matriz_composta = np.dot(matriz_translacao, np.dot(matriz_rotacao, matriz_translacao_inversa))
 
-        _pontos = []
-        for ponto in pontos:
-            _pontos.append(np.dot(matriz_composta, ponto))
+        return np.dot(matriz_composta, ponto)
 
-        return _pontos
-
-    def cisalhamento(self, pontos, cx, cy):
+    def cisalhamento(self, ponto, cx, cy):
         matriz_cisalhamento = np.array([[1, cx, 0],
                                         [cy, 1, 0],
                                         [0, 0, 1]], dtype=object)
 
-        _pontos = []
-        for ponto in pontos:
-            _pontos.append(np.dot(matriz_cisalhamento, ponto))
+        return np.dot(matriz_cisalhamento, ponto)
 
-        return _pontos
-
-    def reflexaoEixo(self, pontos, eixo='x'):
+    def reflexaoEixo(self, ponto, eixo='x'):
         if eixo == 'x':
             matriz_reflexao = np.array([[1, 0, 0],
                                         [0, -1, 0],
@@ -131,13 +92,9 @@ class Transformada2D:
                                         [0, 1, 0],
                                         [0, 0, 1]], dtype=object)
 
-        _pontos = []
-        for ponto in pontos:
-            _pontos.append(np.dot(matriz_reflexao, ponto))
+        return np.dot(matriz_reflexao, ponto)
 
-        return _pontos
-
-    def reflexaoLinha(self, pontos, linha='x'):
+    def reflexaoLinha(self, ponto, linha='x'):
         if linha == 'x':
             matriz_reflexao = np.array([[0, 1, 0],
                                         [1, 0, 0],
@@ -148,8 +105,4 @@ class Transformada2D:
                                         [-1, 0, 0],
                                         [0, 0, 1]], dtype=object)
 
-        _pontos = []
-        for ponto in pontos:
-            _pontos.append(np.dot(matriz_reflexao, ponto))
-
-        return _pontos
+        return np.dot(matriz_reflexao, ponto)
