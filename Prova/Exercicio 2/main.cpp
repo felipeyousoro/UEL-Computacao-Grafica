bca7cfdc1e39fd6ff3455c0b42e31102048efffd
yousorofelipe <42080804+felipeyousoro@users.noreply.github.com>
2023-07-07 15:44:09 -0300

    terra plana da prova, mudo algumas coisas dps

--- Prova/Exercicio 2/main.cpp
+++ Prova/Exercicio 2/main.cpp
@@ -1,54 +1,89 @@
 #include <GL/glut.h>
-#include <cmath>
+#include <math.h>
 
-// Ângulo de rotação da Terra
-float earthRotation = 0.0f;
+#define WIDTH 999
+#define HEIGHT 666
 
-// Ângulo de rotação da Lua
-float moonRotation = 0.0f;
+#define PI 3.14159265
+#define EARTH_RADIUS 1.0
+#define MOON_RADIUS 0.420
+#define EARTH_ROTATION_SPEED 0.177013
+#define MOON_ROTATION_SPEED 4 * 0.69420
+#define MOON_ORBIT_RADIUS 3 * 0.666
 
-// Função de renderização
-void renderScene() {
+float g_earth_angle = 0.0;
+float g_moon_angle = 0.0;
+
+void handle_resize(int width, int height) {
+    glViewport(0, 0, width, height);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(45.0, (float)width / (float)height, 1.0, 200.0);
+}
+
+void draw_earth() {
+    glColor3f(0.0, 0.0, 1.0);  
+    glutWireSphere(EARTH_RADIUS, 20, 20);  
+}
+
+void draw_moon() {
+    glColor3f(1.0, 1.0, 1.0);  
+    glutWireSphere(MOON_RADIUS, 10, 10);  
+}
+
+void render() {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
 
-    // Define a posição da câmera
-    gluLookAt(0.0f, 0.0f, 5.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f);
+    gluLookAt(0.0, 0.0, 5.0,  
+              0.0, 0.0, 0.0,  
+              0.0, 1.0, 0.0);
+    glRotatef(g_earth_angle, 0.0, 1.0, 0.0);
 
-    // Desenha a Terra
-    glColor3f(0.0f, 0.5f, 1.0f);
-    glutSolidSphere(1.0f, 50, 50);
+    draw_earth();
 
-    // Posiciona a Lua em relação à Terra
-    glRotatef(earthRotation, 0.0f, 1.0f, 0.0f);
-    glTranslatef(2.0f, 0.0f, 0.0f);
+    glPushMatrix();
 
-    // Desenha a Lua
-    glColor3f(1.0f, 1.0f, 1.0f);
-    glutSolidSphere(0.3f, 50, 50);
+    glTranslatef(MOON_ORBIT_RADIUS, 0.0, 0.0);
+    glRotatef(g_moon_angle, 0.0, 1.0, 0.0);
 
-    // Atualiza os ângulos de rotação
-    earthRotation += 0.2f;
-    moonRotation += 0.4f;
+    draw_moon();
+
+    glPopMatrix();
 
     glutSwapBuffers();
 }
 
-// Função de atualização da cena
-void update(int value) {
+void update_angles(int value) {
+    g_earth_angle += EARTH_ROTATION_SPEED;
+    if (g_earth_angle > 360.0) {
+        g_earth_angle -= 360.0;
+    }
+
+    g_moon_angle += MOON_ROTATION_SPEED;
+    if (g_moon_angle > 360.0) {
+        g_moon_angle -= 360.0;
+    }
+
     glutPostRedisplay();
-    glutTimerFunc(1, update, 0);
+    glutTimerFunc(16, update_angles, 0); 
 }
 
 int main(int argc, char** argv) {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-    glutInitWindowSize(800, 600);
-    glutCreateWindow("Rotacao da Terra e da Lua");
+    glutInitWindowSize(WIDTH, HEIGHT);
+    glutCreateWindow("Prova Exercicio 2: Terra Plana");
+
     glEnable(GL_DEPTH_TEST);
-    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-    glutDisplayFunc(renderScene);
-    glutTimerFunc(0, update, 0);
+
+    glutDisplayFunc(render);
+    glutReshapeFunc(handle_resize);
+
+    glutTimerFunc(16, update_angles, 0);
+
     glutMainLoop();
+
     return 0;
 }
